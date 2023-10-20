208869ad985cfdadbea51ddb4bb24aceb50c1647
SabbirAhamedMaruf <sabbirahamedmaruf02@gmail.com>
2023-10-20 20:02:15 +0600

    added error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,18 +1,43 @@
+import { Link, useRouteError } from "react-router-dom";
+import { TbHome2 } from "react-icons/tb";
 import Navber from "../Components/Navber";
-
 const ErrorPage = () => {
-  return (
-    <div>
-      <div className="bg-gradient-to-r from-green-400 to-blue-500 h-[95vh] lg:h-[100vh]">
-        {/* <Navber></Navber> */}
+  const error = useRouteError();
 
-        <h1>This is error sdfsdfsffpage</h1>
+  return (
+    <div className="bg-gradient-to-r from-amber-400 to-red-500">
+      <div className="shadow-2xl">
+          <Navber ></Navber>
+      </div>
+      <div className="w-[90%] m-auto h-[100vh]">
+        
+        <div className="w-[90%]  text-center absolute  rounded-3xl mt-32 h-[80vh] py-8">
+          <h1 className="font-kalam font-bold text-3xl text-white md:text-4xl lg:text-6xl mt-8">
+            Where should i go?
+          </h1>
+          <img
+            className="w-[95%] md:w-[60%] lg:w-[25%] m-auto"
+            src="https://i.ibb.co/ykYY2Kn/Error.png"
+          />
+          <div className="font-[500] text-xl text-white md:text-2xl py-8">
+            {error.code === undefined ? (
+              <p>An unknown error occured</p>
+            ) : (
+              <div>
+                <h4>{`Error Code : ${error.status}`}</h4>
+                <h4>{`Error Message : ${error.data}`}</h4>
+              </div>
+            )}
+          </div>
+          <Link>
+            <button className="flex justify-center m-auto w-24 lg:w-32 p-3 border-2 border-white rounded-xl bg-[#fedee1] hover:bg-green-300">
+               <TbHome2 className="inline text-3xl  text-gray-500" />
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 };
 
 export default ErrorPage;
-
-
-// TODO error page not rendering
